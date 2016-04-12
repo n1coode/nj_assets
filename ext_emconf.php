23b9edef657e1a4116c99c3391bf905643379352
n1coode <reg@n1coo.de>
2016-04-13 01:04:04 +0200

    .initial

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+/***************************************************************
+ * Extension Manager/Repository config file for ext: "nj_collection"
+***************************************************************/
+ 
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'njs Assets',
+	'description' => 'Assets for websites and extensions developed by n1coo.de.',
+	'category' => 'plugin',
+	'author' => 'Nico Jatzkowski',
+	'author_email' => 'nj@n1coo.de',
+	'author_company' => 'n1coo.de',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '8.0.1',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'extbase' => '7.6.0-0.0.0',
+			'fluid' => '7.6.0-0.0.0',
+			'typo3' => '7.6.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+);
---
+++ ext_localconf.php
@@ -0,0 +1,14 @@
+<?php
+if(!defined('TYPO3_MODE')) die ('Access denied.');
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'N1coode.'.$_EXTKEY,
+    'Pi1',
+    array(),
+    // non-cacheable actions
+    array()
+);
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][$_EXTKEY] = 'EXT:nj_assets/Classes/Hooks/PageLayoutView.php:N1coode\NjAssets\Hooks\PageLayoutView';
